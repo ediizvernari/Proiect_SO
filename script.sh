223d423092271944a81251ed34d105637debb57f
ediizvernari <edi.izvernari@student.upt.ro>
2023-12-11 13:11:33 +0200

    updated the script

--- script.sh
+++ script.sh
@@ -15,7 +15,7 @@ contor=0
 # Citeste linii pana la end-of-file de la intrarea standard
 while IFS= read -r line; do
     # Verifică dacă linia respectă condițiile
-    if [[ $line =~ ^[[:upper:]].*[a-zA-Z0-9\ ,.!?]$ && $line != *", si"* && $line == *"$char"* && $line != *", $char"* ]]; then
+    if [[ $line =~ ^[[:upper:]].*[a-zA-Z0-9\ ,.!?]$ && $line != *", si"* && $line == *"$char"* && $line != *", $char"* && $line =~ [.,!?]$ ]]; then
         ((contor++))
     fi
 done
